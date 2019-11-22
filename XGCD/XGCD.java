2016d958bdaafcd65b066fe504038e5a0dadaa97
MDubs <marc50000@gmail.com>
2019-11-22 11:41:39 -0500

    Push XGCD code

--- XGCD/XGCD.java
+++ XGCD/XGCD.java
@@ -5,11 +5,26 @@ public class XGCD {
         res.a = a;
         res.b = b;
         runXGCD(res);
-        System.out.println(struct);
+        System.out.println(res);
     }
 
     public void runXGCD(XGCD_Struct struct) {
-        
+        if(struct.b == 0) {
+            struct.d = struct.a;
+            struct.s = 1;
+            struct.t = 0;
+            return;
+        }
+
+        int a_div_b = struct.a / struct.b;
+        int a_mod_b = struct.a % struct.b;
+        struct.a = struct.b;
+        struct.b = a_mod_b;
+        runXGCD(struct);
+        int s_prev = struct.s;
+        int t_prev = struct.t;
+        struct.s = t_prev;
+        struct.t = s_prev - a_div_b*t_prev;
     }
 
     private class XGCD_Struct {
